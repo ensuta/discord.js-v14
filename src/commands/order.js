8198f5048109977dcec4a650be31b39e7d792df4
ensuta <ensuta08130813@gmail.com>
2023-07-04 17:02:53 +0900

    change order cammand

--- src/commands/order.js
+++ src/commands/order.js
@@ -3,45 +3,45 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 const orderCommand = new SlashCommandBuilder()
         .setName('order')
         .setDescription('Order Somethings')
-        .addStringOption((option) => 
-        option
-        .setName('food')
-        .setDescription('the type of food')
-        .setRequired(true)
-        .setChoices(
-            {
-                name: 'Hamburger',
-                value: 'hamburger',
-            },
-            {
-                name: "Cake",
-                value: 'cake',
-            },
-            {
-                name: "Chicken",
-                value: 'chicken',
-            },
-          )
-        ).addStringOption((option) => 
-            option
-                .setName('drink')
-                .setDescription('the type of drink')
-                .setRequired(true)
-                .setChoices(
-                    {
-                        name: 'Water',
-                        value: 'water',
-                    },
-                    {
-                        name: 'Coca-Cola',
-                        value: 'coca-cola',
-                    },
-                    {
-                        name: 'Sprite',
-                        value: 'sprite',
-                    },
-            )
-        );
+//         .addStringOption((option) => 
+//         option
+//         .setName('food')
+//         .setDescription('the type of food')
+//         .setRequired(true)
+//         .setChoices(
+//             {
+//                 name: 'Hamburger',
+//                 value: 'hamburger',
+//             },
+//             {
+//                 name: "Cake",
+//                 value: 'cake',
+//             },
+//             {
+//                 name: "Chicken",
+//                 value: 'chicken',
+//             },
+//           )
+//         ).addStringOption((option) => 
+//             option
+//                 .setName('drink')
+//                 .setDescription('the type of drink')
+//                 .setRequired(true)
+//                 .setChoices(
+//                     {
+//                         name: 'Water',
+//                         value: 'water',
+//                     },
+//                     {
+//                         name: 'Coca-Cola',
+//                         value: 'coca-cola',
+//                     },
+//                     {
+//                         name: 'Sprite',
+//                         value: 'sprite',
+//                     },
+//             )
+//         );
 
        
 
