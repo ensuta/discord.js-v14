8f7577a860e160a6d97241ac1a8781e29a0bd9e8
ensuta <ensuta08130813@gmail.com>
2023-07-07 21:54:25 +0900

    add command

--- src/commands/register.js
+++ src/commands/register.js
@@ -2,6 +2,6 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 
 const registerCommand = new SlashCommandBuilder()
   .setName('register')
-  .setDescription('Register a user to the server officially');
+  .setDescription('register cmd')
 
 export default registerCommand.toJSON();
